398227e3aac452634c8a53b157aa22b6a7f64eb1
Prabhu <123748142+prabhu-88@users.noreply.github.com>
2024-03-15 18:38:49 +0530

    Update template.sh

--- template.sh
+++ template.sh
@@ -2,20 +2,18 @@
 echo -e "Please provide C filename you want to create : \c"
 read -r file
 touch $file.c
-echo '#!/bin/bash' > $file.c
-echo '#purpose : ' >> $file.c
-echo '#created Date : ' `date` >> $file.c
-echo '#Coder : Prabhu' >> $file.c
-echo '#START # ' >> $file.c
-echo '#include<stdio.h>' >> $file.c
-echo '' >> $file.c
-echo 'int main()' >> $file.c
-echo '{' >> $file.c
-echo '' >> $file.c
-echo '' >> $file.c
-echo '}' >> $file.c
-echo '#END # ' >> $file.c
-
+echo '#purpose : ' >>$file.c
+echo '#created Date : ' $(date) >>$file.c
+echo '#Coder : Prabhu' >>$file.c
+echo '#######--------START------######### # ' >>$file.c
+echo '#include<stdio.h>' >>$file.c
+echo '' >>$file.c
+echo 'int main()' >>$file.c
+echo '{' >>$file.c
+echo '' >>$file.c
+echo '' >>$file.c
+echo '}' >>$file.c
+echo '#####---------END-------######### ' >>$file.c
 
 #chmod 777 template
 #sudo mv template /bin/
